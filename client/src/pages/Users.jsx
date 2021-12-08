1f755bc7294c0699abc7ec4626ebf4f79fa6b670
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2021-12-08 17:29:13 +0600

    history.push()/navigate functional

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,22 +1,21 @@
 import React, {useEffect, useState} from 'react'
-
+import { useNavigate  } from "react-router-dom";
 
 function Users() {
     const [users, setUsers] = useState([])
-
+    let navigate = useNavigate ();
     useEffect(() => {
-
         fetch('/users')
         .then(res => res.json())
         .then(list => setUsers(list) )
         .catch(err=>console.log("api call failed"))
-    
     }, [])
 
     return (
-        <div style={{background:"blue", height:"100vh"}}>
+        <div style={{background:"blue", height:"100vh", color:"white" }}>
             <h1>Users page</h1>
             { users.map( user => <p>{ user }</p>)}
+            <button onClick={()=>navigate("/")}>Go to home</button>
         </div>
     )
 }
